ad45d28e09c07b274649e9b31680f877221b93af
padd0017 <padd0017@algonquinlive.com>
2025-06-12 19:38:45 -0400

    Assignment completed

---
+++ ToDoAppMaui/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using ToDoAppMaui.Views;
+
+namespace ToDoAppMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            // Start with a NavigationPage hosting our HomePage
+            return new Window(new NavigationPage(new HomePage()));
+        }
+    }
+}
