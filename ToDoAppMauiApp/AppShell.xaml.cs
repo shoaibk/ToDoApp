d7dea25c88eb66a46aab3ecdbf4d4dd696a6f7d8
Adhithyan Kizhuppully Ullas <kizh0008@algonquinlive.com>
2025-06-13 22:31:56 -0400

    compleated

---
+++ ToDoAppMauiApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoAppMauiApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ToDoAppMauiApp/LoginPage.xaml.cs
@@ -0,0 +1,27 @@
+using System.Net.Http.Json;
+namespace ToDoAppMauiApp;
+
+public partial class LoginPage : ContentPage
+{
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void LoginClicked(object? sender, EventArgs e)
+    {
+        var user = new { Username = usernameEntry.Text, Password = passwordEntry.Text };
+        using var client = new HttpClient();
+        var response = await client.PostAsJsonAsync(" http://localhost:5097/api/auth/login", user);
+
+        if (response.IsSuccessStatusCode)
+        {
+            await DisplayAlert("Success", "You have successfully logged in", "OK");
+        }
+        else
+        {
+            await DisplayAlert("Error", "Something went wrong", "OK");
+        }
+        
+    }
+}
