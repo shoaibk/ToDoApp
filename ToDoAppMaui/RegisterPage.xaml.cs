964706adf9be5933e2e88a3a7736eef034773275
Anita Joseph <jose0319@algonquinlive.com>
2025-06-15 21:22:36 -0400

    completed assignment

---
+++ ToDoAppMaui/RegisterPage.xaml.cs
@@ -0,0 +1,25 @@
+using System.Net.Http.Json;
+namespace ToDoAppMaui;
+public partial class RegisterPage : ContentPage
+{
+    public RegisterPage()
+    {
+        InitializeComponent();
+    }
+    private async void RegisterClicked(object? sender, EventArgs e)
+    {
+        var user = new { Username = usernameEntry.Text, Password = passwordEntry.Text };
+        using var client = new HttpClient();
+        var response = await client.PostAsJsonAsync("http://localhost:5097/api/auth/register", user);
+
+        if (response.IsSuccessStatusCode)
+        {
+            await DisplayAlert("Success", "Your account has been created", "OK");
+        }
+        else
+        {
+            await DisplayAlert("Error", "Something went wrong", "OK");
+        }
+        
+    }
+}
