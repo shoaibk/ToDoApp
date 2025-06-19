ae9cca1f6cc444cd66951d548417194a0b1a43f4
Adhithyan Kizhuppully Ullas <kizh0008@algonquinlive.com>
2025-06-19 19:56:49 -0400

    compleated

--- ToDoAppMaui/Views/TodoListPage.xaml.cs
+++ ToDoAppMaui/Views/TodoListPage.xaml.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ToDoAppMaui.Models;
 using ToDoAppMaui.Services;
 
@@ -10,18 +5,49 @@ namespace ToDoAppMaui.Views;
 
 public partial class TodoListPage : ContentPage
 {
-    private User _user;
+    private  User _user;
     private ApiService _api = new();
+
     public TodoListPage(User user)
     {
         InitializeComponent();
         _user = user;
-        LoadTodos();
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await LoadTodos();
     }
 
     private async Task LoadTodos()
     {
         var todos = await _api.GetTodos(_user);
         todoList.ItemsSource = todos;
+    }
+
+    private async void OnAddTodoClicked(object sender, EventArgs e)
+    {
+        var title = todoEntry.Text?.Trim();
+
+        if (string.IsNullOrWhiteSpace(title))
+        {
+            await DisplayAlert("Error", "Todo title cannot be empty", "OK");
+            return;
+        }
+
+        var todo = new Todo { Title = title, IsCompleted = false };
+
+        var success = await _api.AddTodo(_user, todo);
+
+        if (success)
+        {
+            todoEntry.Text = string.Empty; 
+            await LoadTodos(); 
+        }
+        else
+        {
+            await DisplayAlert("Error", "Failed to add todo", "OK");
+        }
     }
 }
