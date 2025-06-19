b9d5cb572f218c57133546ecb1967d2d97bc81c3
Shoaib Khan <shb.khan@gmail.com>
2025-06-19 05:39:29 -0400

    Add Maui App, Login, Registration and TodoList Pages.

---
+++ ToDoAppMaui/Models/Todo.cs
@@ -0,0 +1,7 @@
+namespace ToDoAppMaui.Models;
+
+public class Todo
+{
+    public string Title { get; set; }
+    public bool IsCompleted { get; set; }
+}
---
+++ ToDoAppMaui/Views/TodoListPage.xaml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoAppMaui.Models;
+using ToDoAppMaui.Services;
+
+namespace ToDoAppMaui.Views;
+
+public partial class TodoListPage : ContentPage
+{
+    private User _user;
+    private ApiService _api = new();
+    public TodoListPage(User user)
+    {
+        InitializeComponent();
+        _user = user;
+        LoadTodos();
+    }
+
+    private async Task LoadTodos()
+    {
+        var todos = await _api.GetTodos(_user);
+        todoList.ItemsSource = todos;
+    }
+}
