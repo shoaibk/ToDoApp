6bf7f1d4ef2673ab6c3717c6ea3ac403a3711072
Cemilcan Gorur <goru0003@algonquinlive.com>
2025-06-19 18:57:01 -0400

    assignment completed

---
+++ ToDoApp/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http.Json;
+
+namespace ToDoApp;
+
+public partial class MainPage : ContentPage
+{
+    HttpClient client = new HttpClient();
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnLoginClicked(object sender, EventArgs e)
+    {
+        var user = new
+        {
+            Username = UsernameEntry.Text,
+            Password = PasswordEntry.Text
+        };
+
+        try
+        {
+            var response = await client.PostAsJsonAsync("http://localhost:5097/api/auth/login", user);
+
+            if (response.IsSuccessStatusCode)
+            {
+                await DisplayAlert("Success", "Login successful", "OK");
+            
+            }
+            else
+            {
+                var msg = await response.Content.ReadAsStringAsync();
+                await DisplayAlert("Error", msg, "OK");
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "OK");
+        }
+    }
+
+    private async void OnGoToRegisterClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new RegisterPage());
+    }
+}
---
+++ ToDoApp/Register.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http.Json;
+
+namespace ToDoApp;
+
+public partial class RegisterPage : ContentPage
+{
+    HttpClient client = new HttpClient();
+
+    public RegisterPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnRegisterClicked(object sender, EventArgs e)
+    {
+        var user = new
+        {
+            Username = UsernameEntry.Text,
+            Password = PasswordEntry.Text
+        };
+
+        try
+        {
+            var response = await client.PostAsJsonAsync("http://localhost:5097/api/auth/register", user);
+
+            if (response.IsSuccessStatusCode)
+            {
+                await DisplayAlert("Success", "User created", "OK");
+                await Navigation.PopAsync(); 
+            }
+            else
+            {
+                var msg = await response.Content.ReadAsStringAsync();
+                await DisplayAlert("Error", msg, "OK");
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "OK");
+        }
+    }
+}
