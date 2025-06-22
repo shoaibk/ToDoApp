6c8dd45ea9dc4394c654da236e9cc5cc7e6c3f00
Brodie Lewis <lewi0239@algonquinlive.com>
2025-06-22 12:46:18 -0400

    Completed AddTodoPage, and clean solution built successfully

--- ToDoAppMaui/Views/AddToDoPage.xaml.cs
+++ ToDoAppMaui/Views/AddToDoPage.xaml.cs
@@ -1,6 +1,46 @@
+using ToDoAppMaui.Services;
+using ToDoAppMaui.Models;
+using System;
+
 namespace TodoAppMaui.Views;
 
-public class AddToDoPage
+public partial class AddToDoPage : ContentPage
 {
-    
+    private readonly User _user;
+    private readonly ApiService _api = new();
+
+    public AddToDoPage(User user)
+    {
+        InitializeComponent();
+        _user = user;
+    }
+
+    private async void OnAddTodoClicked(object sender, EventArgs e)
+    {
+        string content = TodoEntry.Text;
+
+        if (content == null || content.Trim() == "")
+        {
+            errLabel.Text = "Please enter text...";
+            errLabel.IsVisible = true;
+            return;
+        }
+
+        var newTodo = new Todo
+        {
+            Title = content.Trim(),
+            IsCompleted = false
+        };
+
+        var success = await _api.AddTodo(_user, newTodo);
+        if (success)
+        {
+            await Navigation.PopAsync();
+        }
+        else
+        {
+            errLabel.Text = "Todo service failed, please try again.";
+            errLabel.IsVisible = true;
+        }
+    }
 }
