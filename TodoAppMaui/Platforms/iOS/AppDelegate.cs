90dcd0827091e2735adf50f9f9b16f535f3c4bb5
Brodie Lewis <lewi0239@algonquinlive.com>
2025-06-22 12:00:36 -0400

    Fixing name error in dir for ToDoAppMaui

--- TodoAppMaui/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace ToDoAppMaui;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
